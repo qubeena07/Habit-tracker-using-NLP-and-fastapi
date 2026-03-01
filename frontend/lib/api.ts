65c317ccd6947e08198378b78c7a3f447c3fe2d0
Dipika Ranabhat <dipikaranabhat@Mac.hitronhub.home>
2026-03-01 13:01:24 -0600

    updated base url

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,6 +1,6 @@
 import { HabitRecord, AuthResponse, User}  from "@/types";
 
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://habit-tracker-using-nlp-and-fastapi.onrender.com";
 
 const getToken = () =>{
     if (typeof window !== "undefined") {
